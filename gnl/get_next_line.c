07b70adef374ad9cd134a1fea72a791779cf96a5
Come FOUNDOULIS <cofoundo@e1r10p3.42.fr>
2019-11-26 20:15:36 +0100

    gnl

---
+++ gnl/get_next_line.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cofoundo <cofoundo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/18 17:05:00 by cofoundo          #+#    #+#             */
+/*   Updated: 2019/11/26 20:14:39 by cofoundo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t		ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = -1;
+	while (s[++i])
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*dst;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	if (!(dst = malloc(sizeof(char) * (i +1))))
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		dst[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		dst[++i - 1] = s2[j];
+	free(s2);
+	free(s1);
+	dst[i] = '\0';
+	return (dst);
+}
+//faire une structure
+int		get_next_line(int fd, char **line)
+{
+	static	char	BUFFER[OPEN_MAX][BUFFER_SIZE];
+	char			*tmp;
+	int				i;
+	int				count;
+
+	count = 0;
+	if (fd < 0 || BUFF_SIZE < 1)
+		return (-1);
+	if (!BUFFER)
+	{
+		if ((i = read()
+
+	}
+	else
+	{
+	}
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cofoundo <cofoundo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/18 17:05:18 by cofoundo          #+#    #+#             */
+/*   Updated: 2019/11/26 19:45:25 by cofoundo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
