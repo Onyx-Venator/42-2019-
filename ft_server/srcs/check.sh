43f0f5d512ccbeec48c0fac3bdcdaaed5137c0d6
Come FOUNDOULIS <cofoundo@e1r8p14.42.fr>
2020-07-31 14:43:38 +0200

    test

--- ft_server/srcs/check.sh
+++ ft_server/srcs/check.sh
@@ -1,6 +1,7 @@
-if $INDEX = 1
-@mv /tmp/nginxoff.conf /tmp/nginx.conf
+if [ 1 ]
+then
+mv /tmp/nginxoff.conf /tmp/nginx.conf
 else
-@mv /tmp/nginxon.conf /tmp/nginx.conf
-endif
-@mv /tmp/nginx.conf /etc/nginx/sites-available/localhost
+mv /tmp/nginxon.conf /tmp/nginx.conf
+fi
+mv /tmp/nginx.conf /etc/nginx/sites-available/localhost
