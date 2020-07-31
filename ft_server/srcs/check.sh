5320404a08afcc34270a34360edfc0c78e3a9c8b
Come FOUNDOULIS <cofoundo@e1r8p14.42.fr>
2020-07-31 16:50:08 +0200

    fini

--- ft_server/srcs/check.sh
+++ ft_server/srcs/check.sh
@@ -1,7 +1,1 @@
-if [ $1 = "0" ]
-then
-mv /tmp/nginxoff.conf /tmp/nginx.conf
-else
-mv /tmp/nginxon.conf /tmp/nginx.conf
-fi
-mv /tmp/nginx.conf /etc/nginx/sites-available/localhost
+
--- ft_server/srcs/exec.sh
+++ ft_server/srcs/exec.sh
@@ -1,3 +1,10 @@
+if [ $AUTOINDEX = 0 ]
+then
+mv /tmp/nginxoff.conf /tmp/nginx.conf
+else
+mv /tmp/nginxon.conf /tmp/nginx.conf
+fi
+mv /tmp/nginx.conf /etc/nginx/sites-available/localhost
 service php7.3-fpm start
 service nginx restart
 service mysql restart
